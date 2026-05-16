fb831f1fa102add5ae9b37d996df47c2a640ae79
dev-pustek <devpustek@gmail.com>
2026-05-16 18:44:36 +0700

    feat: implement unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -10,7 +9,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     reporters: ['default', 'html'],
-    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'tests/selenium/**'],
+    exclude: [...configDefaults.exclude, 'tests/selenium/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
